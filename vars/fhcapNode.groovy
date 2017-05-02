c4c71b05db1c9cca2550e9df75f2c8c6d5b5353a
Michael Nairn <mnairn@redhat.com>
2017-05-02 20:57:14 +0100

    Update fhcapNode:
    * Add label option, default is 'ruby-fhcap'
    * Add sshagent and ansiColour
    * Add git user name and email options, configures global git user if provided
    * Add fhcapRepos option to automatically configure repos.

--- vars/fhcapNode.groovy
+++ vars/fhcapNode.groovy
@@ -2,40 +2,58 @@
 
 def call(Map parameters = [:], body) {
 
-    node('ruby-fhcap') {
+    def label = parameters.get('label', 'ruby-fhcap')
 
-        env.RUBYOPT="-W0"
+    node(label) {
 
         def installLatest = parameters.get('installLatest', false)
         def credentialsId = parameters.get('credentialsId', 'jenkinsgithub')
         def configFile = parameters.get('configFile', "${WORKSPACE}/fhcap.json")
         def gitRepo = parameters.get('gitRepo', null)
         def gitRef = parameters.get('gitRef', 'master')
+        def gitUserName = parameters.get('gitUserName', null)
+        def gitUserEmail = parameters.get('gitUserEmail', null)
+        def fhcapRepos = parameters.get('fhcapRepos', null)
 
         step([$class: 'WsCleanup'])
-
-        if(gitRepo) {
-            dir('fhcap-cli') {
-                checkoutGitRepo {
-                    repoUrl = gitRepo
-                    branch = gitRef
-                }
-                sh "rake install"
-            }
-        } else {
-            if(installLatest) {
-                sh "gem install fhcap-cli --no-ri --no-rdoc"
-            }
-        }
-        sh "fhcap --version"
-
-        env.PATH = "${PATH}:/home/jenkins/bin"
-        env.FHCAP_CFG_FILE = configFile
-
         sshagent([credentialsId]) {
-            sh "fhcap setup --repos-dir ${WORKSPACE} --fh-src-dir ${WORKSPACE}"
-        }
 
-        body()
+            if (gitUserName && gitUserEmail) {
+                sh "git config --global user.name \"${gitUserName}\""
+                sh "git config --global user.email \"${gitUserEmail}\""
+            }
+
+            ansiColor('xterm') {
+                env.RUBYOPT = "-W0"
+
+                if (gitRepo) {
+                    dir('fhcap-cli') {
+                        checkoutGitRepo {
+                            repoUrl = gitRepo
+                            branch = gitRef
+                        }
+                        sh "rake install"
+                    }
+                } else {
+                    if (installLatest) {
+                        sh "gem install fhcap-cli --no-ri --no-rdoc"
+                    }
+                }
+                sh "fhcap --version"
+
+                env.PATH = "${PATH}:/home/jenkins/bin"
+                env.FHCAP_CFG_FILE = configFile
+
+                sh "fhcap setup --repos-dir ${WORKSPACE} --fh-src-dir ${WORKSPACE}"
+
+                if (fhcapRepos) {
+                    fhcapRepoAddBatch {
+                        repos = fhcapRepos
+                    }
+                }
+
+                body()
+            }
+        }
     }
 }
