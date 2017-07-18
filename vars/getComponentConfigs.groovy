827bd8c9133ed6ace6fc1961e6a9e6ae09e4aeb3
Gerard Ryan <gerard@ryan.lt>
2017-07-18 12:59:08 +0100

    ✨ Add labels property to componentConfigs entries
    JIRA: https://issues.jboss.org/browse/RHMAP-16641

--- vars/getComponentConfigs.groovy
+++ vars/getComponentConfigs.groovy
@@ -29,6 +29,7 @@ def call(componentConfigs, configGitRepo, configGitRef) {
                     componentConfigs[componentName]['cookbook'] = config['cookbook'] ?: componentName
                     componentConfigs[componentName]['buildType'] = config['buildType'] ?: 'node'
                     componentConfigs[componentName]['distCmd'] = config['distCmd']
+                    componentConfigs[componentName]['labels'] = config?.labels ?: {}
                     componentConfigs[componentName]['buildJobName'] = config['buildJobName'] ?: "build_any_jenkinsfile"
                     componentConfigs[componentName]['gitUrl'] = "git@github.com:${componentConfigs[componentName]['gitHubOrg']}/${componentConfigs[componentName]['repoName']}.git"
                     componentConfigs[componentName]['gitHubUrl'] = "https://github.com/${componentConfigs[componentName]['gitHubOrg']}/${componentConfigs[componentName]['repoName']}"
