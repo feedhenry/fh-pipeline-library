2e81677b688c1b8e117f2c603324c072d8d185cd
Michael Nairn <mnairn@redhat.com>
2017-08-09 13:42:26 +0100

    Remove sshagent and git config setup from branchRepo and tagRepo helpers (Slows things down when running multiple commands in parallel)

--- vars/tagRepo.groovy
+++ vars/tagRepo.groovy
@@ -7,35 +7,30 @@ def call(String gitRepoUrl, String checkoutGitBranch, String checkoutDir, String
         targetDir = checkoutDir
     }
     dir(checkoutDir) {
-        sshagent(['jenkinsgithub']) {
-            //How do we get this from the global config? Is sshagent the only way to do this currently or is there a git pipeline step?
-            sh "git config user.name \"Jenkins\""
-            sh "git config user.email \"jenkins@wendy.feedhenry.net\""
 
-            body()
+        body()
 
-            def existingTagCommitHash = sh(returnStdout: true, script: "git ls-remote origin refs/tags/${tagName} | cut -f 1").trim()
-            def tagCommitHash = sh(returnStdout: true, script: "git rev-parse ${tagGitRef}").trim()
+        def existingTagCommitHash = sh(returnStdout: true, script: "git ls-remote origin refs/tags/${tagName} | cut -f 1").trim()
+        def tagCommitHash = sh(returnStdout: true, script: "git rev-parse ${tagGitRef}").trim()
 
-            if (existingTagCommitHash) {
-                if (existingTagCommitHash == tagCommitHash) {
-                    print "Tag ${tagName} already exists and tagCommitHash(${tagCommitHash}) is the same, can continue!"
-                } else {
-                    print "Tag ${tagName} already exists but tagCommitHash(${tagCommitHash}) is not the same, can't continue!"
-                    sh('exit 1')
-                }
+        if (existingTagCommitHash) {
+            if (existingTagCommitHash == tagCommitHash) {
+                print "Tag ${tagName} already exists and tagCommitHash(${tagCommitHash}) is the same, can continue!"
             } else {
-                print "Creating new tag ${tagName} at ${tagCommitHash}"
-                sh "git tag ${tagName} ${tagCommitHash}"
+                print "Tag ${tagName} already exists but tagCommitHash(${tagCommitHash}) is not the same, can't continue!"
+                sh('exit 1')
             }
+        } else {
+            print "Creating new tag ${tagName} at ${tagCommitHash}"
+            sh "git tag ${tagName} ${tagCommitHash}"
+        }
 
-            if (params.dryRun) {
-                print "Would push ${checkoutGitBranch} to ${gitRepoUrl}"
-                print "Would push ${tagName} to ${gitRepoUrl}"
-            } else {
-                sh "git push origin HEAD:${checkoutGitBranch}"
-                sh "git push origin ${tagName}"
-            }
+        if (params.dryRun) {
+            print "Would push ${checkoutGitBranch} to ${gitRepoUrl}"
+            print "Would push ${tagName} to ${gitRepoUrl}"
+        } else {
+            sh "git push origin HEAD:${checkoutGitBranch}"
+            sh "git push origin ${tagName}"
         }
     }
 }
