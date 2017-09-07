76c999c50d90401b2aebdcc55dafb9819c1f7e9d
Gerard Ryan <gerard@ryan.lt>
2017-09-07 12:20:56 +0100

    🔥 Stop dealing with dockerhub secrets
    JIRA: https://issues.jboss.org/browse/RHMAP-17489
    
    We've experienced cases where if an error is thrown in this section of
    code, passwords are exposed in the logs.

--- vars/dockerBinaryBuild.groovy
+++ vars/dockerBinaryBuild.groovy
@@ -1,8 +1,6 @@
 #!/usr/bin/groovy
 
 def call(String name, String version, String dockerHubOrg, String dockerHubRepo = name, String credentialId = "dockerhubjenkins", String fromDir = './docker', Closure body = {}) {
-    openshiftEnsureDockerhubCredentials(dockerHubOrg, credentialId)
-
     def pushConfig = [
             "kind": "DockerImage",
             "name": "docker.io/${dockerHubOrg}/${dockerHubRepo}:${version}-${env.BUILD_NUMBER}"
--- vars/openshiftEnsureDockerhubCredentials.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/groovy
-
-def call(String dockerHubOrg, String credentialId) {
-    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: credentialId, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-        createOpenshiftDockerSecret "docker.io/${dockerHubOrg}", "${env.USERNAME}", "${env.PASSWORD}", "fh.team.eng-group@redhat.com", "${dockerHubOrg}"
-    }
-}
