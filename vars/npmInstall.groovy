79ad3774486c2808caacff27a9b63edf158fd311
Gerard Ryan <gerard@ryan.lt>
2017-09-11 15:14:32 +0100

    🚑 Stop depending on master of license-reporter

--- vars/npmInstall.groovy
+++ vars/npmInstall.groovy
@@ -9,7 +9,7 @@ def call(body) {
     sh '''
         npm install --production
         npm ls
-        npm install -g https://github.com/bucharest-gold/license-reporter
+        npm install -g https://github.com/bucharest-gold/license-reporter#720d739a94ae4190cbc8c078f47f018b8fd36d90
         license-reporter --ignore-version-range --all --silent --file licenses.xml
         npm install
         npm install grunt-cli -g
