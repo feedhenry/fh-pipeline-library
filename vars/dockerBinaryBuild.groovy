e636f3f4b8eaefa2fa09a4072209fcd212b5674e
Gerard Ryan <gerard@ryan.lt>
2017-09-26 10:21:28 +0100

    🐛 Add pull secret to BC

--- vars/dockerBinaryBuild.groovy
+++ vars/dockerBinaryBuild.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/groovy
 
-def call(String name, String version, String dockerHubOrg, String dockerHubRepo = name, String credentialId = "dockerhubjenkins", String fromDir = './docker', Closure body = {}) {
+def call(String name, String version, String dockerHubOrg, String dockerHubRepo = name, String dockerSecret = "dockerhub", String fromDir = './docker', Closure body = {}) {
     def pushConfig = [
             "kind": "DockerImage",
             "name": "docker.io/${dockerHubOrg}/${dockerHubRepo}:${version}-${env.BUILD_NUMBER}"
     ]
 
-    openshiftEnsureBinaryBuild("${name}-bc", pushConfig)
+    openshiftEnsureBinaryBuild("${name}-bc", pushConfig, dockerSecret)
     body()
     sh "oc start-build ${name}-bc --follow=true --from-dir ${fromDir}"
 }
