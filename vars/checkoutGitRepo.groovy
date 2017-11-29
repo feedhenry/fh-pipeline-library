77c6ad26c631faf11ece4f240e885966557113ef
Vitalii Chepeliuk <vchepeli@redhat.com>
2017-11-29 11:31:23 +0100

    config git remotes for upstream/downstream

--- vars/checkoutGitRepo.groovy
+++ vars/checkoutGitRepo.groovy
@@ -1,23 +1,42 @@
 #!/usr/bin/groovy
 
-def checkoutWithConfig(config) {
-   def noTags = config.noTags  ?: false
-   def shallow = config.shallow  ?: false
-   
-   checkout([$class: 'GitSCM',
-            branches: [[name: config.branch]],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [
-                    [$class: 'RelativeTargetDirectory', relativeTargetDir: config.targetDir ?: '.'],
-                    [$class: 'CloneOption', noTags: noTags, reference: '', shallow: shallow]
-            ],
-            submoduleCfg: [],
-            userRemoteConfigs: [
-                    [
-                            credentialsId: 'jenkinsgithub',
-                            url: config.repoUrl
-                    ]
+/**
+ * @param config {
+ *     noTags = pull all tags from remote repo or not?
+ *     shallow = shallow copy of remote repository
+ *     localBranch = clone remote branch into local branch
+ *     targetDir =  what directory files should be cloned in
+ *     remoteBranches = subset of remote branches you want to pull
+ * }
+ * @return void
+ */
+void checkoutWithConfig(config) {
+    def noTags = config.noTags ?: false
+    def shallow = config.shallow ?: false
+    def localBranch = config.localBranch ?: 'master'
+    def relativeTargetDir = config.targetDir ?: '.'
+    def remoteBranches = config.remoteBranches ?: [
+            [
+                    name: config.branch ?: '*/master'
             ]
+    ]
+    def userRemoteConfigs = config.userRemoteConfigs ?: [
+            [
+                    credentialsId: 'jenkinsgithub',
+                    url          : config.repoUrl,
+            ]
+    ]
+
+    checkout([$class                           : 'GitSCM',
+              branches                         : remoteBranches,
+              doGenerateSubmoduleConfigurations: false,
+              extensions                       : [
+                      [$class: 'LocalBranch', localBranch: localBranch],
+                      [$class: 'RelativeTargetDirectory', relativeTargetDir: relativeTargetDir],
+                      [$class: 'CloneOption', noTags: noTags, reference: '', shallow: shallow]
+              ],
+              submoduleCfg                     : [],
+              userRemoteConfigs                : userRemoteConfigs
     ])
 }
 
@@ -28,17 +47,16 @@ def call(body) {
     body.delegate = config
     body()
 
-    def backupBranch = config.backupBranch  ?: false
+    def backupBranch = config.backupBranch ?: false
 
     try {
-      checkoutWithConfig(config)
+        checkoutWithConfig(config)
     } catch (Exception e) {
         if (backupBranch) {
-          config.branch = backupBranch
-          checkoutWithConfig(config)
+            config.branch = backupBranch
+            checkoutWithConfig(config)
         } else {
-          throw e
+            throw e
         }
     }
-    
 }
