7f2188caf71c49294e63fbab7b51c5ea53b7eb24
Vitalii Chepeliuk <vchepeli@redhat.com>
2017-12-21 15:24:51 +0100

    RHMAP-17534 Managing ENV Path in Jenkins Pipelines

--- vars/fhcapNode.groovy
+++ vars/fhcapNode.groovy
@@ -25,58 +25,54 @@ def call(Map parameters = [:], body) {
 
     node(withLabels(labels)) {
 
-        def utils = new Utils()
-        def installLatest = parameters.get('installLatest', false)
-        def credentialsId = parameters.get('credentialsId', 'jenkinsgithub')
-        def configFile = parameters.get('configFile', "${WORKSPACE}/fhcap.json")
-        def gitRepo = parameters.get('gitRepo', null)
-        def gitRef = parameters.get('gitRef', 'master')
-        def gitUserName = parameters.get('gitUserName', null)
-        def gitUserEmail = parameters.get('gitUserEmail', null)
-        def fhcapRepos = parameters.get('fhcapRepos', null)
-        def workDir = parameters.get('workDir', '.')
-        def setupOpts = parameters.get('setupOpts', [:])
-        def defaultSetupOts = ['--repos-dir': WORKSPACE, '--fh-src-dir': WORKSPACE, '--knife-dir': WORKSPACE]
+        def configFile = parameters.get('configFile', "${env.WORKSPACE}/fhcap.json")
+        withEnv(["PATH+=/home/jenkins/bin", "RUBYOPT=-W0", "FHCAP_CFG_FILE=${configFile}"]) {
+            def utils = new Utils()
+            def installLatest = parameters.get('installLatest', false)
+            def credentialsId = parameters.get('credentialsId', 'jenkinsgithub')
+            def gitRepo = parameters.get('gitRepo', null)
+            def gitRef = parameters.get('gitRef', 'master')
+            def gitUserName = parameters.get('gitUserName', null)
+            def gitUserEmail = parameters.get('gitUserEmail', null)
+            def fhcapRepos = parameters.get('fhcapRepos', null)
+            def workDir = parameters.get('workDir', '.')
+            def setupOpts = parameters.get('setupOpts', [:])
+            def defaultSetupOts = ['--repos-dir': WORKSPACE, '--fh-src-dir': WORKSPACE, '--knife-dir': WORKSPACE]
 
-        String setupOptsStr = utils.mapToOptionsString(defaultSetupOts + setupOpts, ' ')
+            String setupOptsStr = utils.mapToOptionsString(defaultSetupOts + setupOpts, ' ')
 
-        step([$class: 'WsCleanup'])
-        sshagent([credentialsId]) {
+            step([$class: 'WsCleanup'])
+            sshagent([credentialsId]) {
 
-            sh "rm -rf ${HOME}/fhcap"
+                sh "rm -rf ${HOME}/fhcap"
 
-            if (gitUserName && gitUserEmail) {
-                sh "git config --global user.name \"${gitUserName}\""
-                sh "git config --global user.email \"${gitUserEmail}\""
-            }
-
-            ansiColor('xterm') {
-                env.RUBYOPT = "-W0"
-
-                if (gitRepo) {
-                    checkoutFhcapCli(gitRepo, gitRef, true)
-                } else {
-                    //This is only here to get git clones to work correctly, and avoid 'Host key verification failed' errors
-                    checkoutFhcapCli()
-                    if (installLatest) {
-                        sh "gem install fhcap-cli --no-ri --no-rdoc"
-                    }
-                }
-                sh "fhcap --version"
-
-                env.PATH = "${PATH}:/home/jenkins/bin"
-                env.FHCAP_CFG_FILE = configFile
-
-                sh "yes | fhcap setup ${setupOptsStr}"
-
-                if (fhcapRepos) {
-                    fhcapRepoAddBatch {
-                        repos = fhcapRepos
-                    }
+                if (gitUserName && gitUserEmail) {
+                    sh "git config --global user.name \"${gitUserName}\""
+                    sh "git config --global user.email \"${gitUserEmail}\""
                 }
 
-                dir(workDir) {
-                    body()
+                ansiColor('xterm') {
+                    if (gitRepo) {
+                        checkoutFhcapCli(gitRepo, gitRef, true)
+                    } else {
+                        //This is only here to get git clones to work correctly, and avoid 'Host key verification failed' errors
+                        checkoutFhcapCli()
+                        if (installLatest) {
+                            sh "gem install fhcap-cli --no-ri --no-rdoc"
+                        }
+                    }
+                    sh "fhcap --version"
+                    sh "yes | fhcap setup ${setupOptsStr}"
+
+                    if (fhcapRepos) {
+                        fhcapRepoAddBatch {
+                            repos = fhcapRepos
+                        }
+                    }
+
+                    dir(workDir) {
+                        body()
+                    }
                 }
             }
         }
