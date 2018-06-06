8888f2d2b2acfde8793ad219b3b25c95d7c637ab
Gerard Ryan <gerard@ryan.lt>
2018-06-06 16:17:33 +0100

    ✨ Add awaitBuild method to Koji class

--- src/org/feedhenry/Koji.groovy
+++ src/org/feedhenry/Koji.groovy
@@ -30,6 +30,22 @@ class Koji implements Serializable {
     }
 
     @NonCPS
+    Map<String, String> awaitBuild(String nvr, Integer maxSleepSeconds = 14400) {
+        Map<String, String> build = getBuild(nvr)
+        Integer sleepSeconds = 1
+
+        while (build?.state == BUILD_INPROGRESS &&
+               sleepSeconds < maxSleepSeconds + 1) {
+            Thread.sleep(sleepSeconds)
+            sleepSeconds = sleepSeconds * 2
+
+            build = getBuild(nvr)
+        }
+
+        return build ?: [:]
+    }
+
+    @NonCPS
     List<Map<String, String>> listBuilds(Integer packageID) {
         final XMLRPCServerProxy koji = new XMLRPCServerProxy(url, true)
         return koji.listBuilds(packageID)
